56363426da44014ec11ec6f7a6038a67e68bb9dc
ProfRoxas <kuroda.taiki@gmail.com>
2019-03-16 14:12:37 +0100

    Added tick and Spawner to Map

--- src/snake/entities/Entity.java
+++ src/snake/entities/Entity.java
@@ -1,5 +1,15 @@
 package snake.entities;
 
 public interface Entity{
-    
+    /**
+     * This Handles the game logic on every update. <p>
+     * The TableMap will iterate over this on every tick
+     */
+    public void tick();
+    /**
+     * The function that handles collision.
+     * 
+     * @param e The Entity it collides with
+     */
+    public void collidesWith(Entity e);
 }
--- src/snake/tools/TableMap.java
+++ src/snake/tools/TableMap.java
@@ -2,6 +2,8 @@ package snake.tools;
 
 import java.awt.Point;
 import java.lang.annotation.Documented;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 import snake.entities.Entity;
@@ -10,7 +12,9 @@ public class TableMap {
     private final int __x;
     private final int __y;
     private final Entity __field[][];
-    private final int __difficulty;
+    private final Random __random;
+    /** The objects the map holds, for quicker tick() iterating */
+    private List<Entity> __container;
 
     /**
      * Create a new Entity field based on the parameters.
@@ -19,15 +23,15 @@ public class TableMap {
      * @param my Maximum Y, the height
      * @param ss Snake Size, the initial size of the Snake
      * @param wc Wall Count, the initial count of walls
-     * @param ds Difficulty Setting, determines entity spawn logic
      * @param rs Random Seed, the seed for the random generator
      */
-    public TableMap(int mx, int my, int ss, int wc, int ds, long rs) {
-        Random r = new Random(rs);
+    public TableMap(int mx, int my, int ss, int wc, long rs) {
+        __random = new Random(rs);
         __x = mx;
         __y = mx;
-        __difficulty = ds;
         __field = new entity[x][y];
+
+        __container = new LinkedList<>();
     }
     
     /**
@@ -52,11 +56,42 @@ public class TableMap {
         if(__field[p.x][p.y] != null)
             return false;
         __field[p.x][p.y] = e;
+        /* [TODO] Put snake parts in the head for quicker remove */
+        __container.add(e);
         return true;
     }
+    /**
+     * Remove a tile from the map
+     * 
+     * @param p the tile you want it from
+     * 
+     * @return The entity that occupied the tile
+     */
     public Entity remove(Point p) {
         entity r = __field[p.x][p.y];
+        __container.remove(r);
         __field[p.x][p.y] = null;
         return r;
+    }
+    /**
+     * Calls the Tick function on every Entity of the map for game logic update
+     */
+    public void tick() {
+        __container.forEach(entity->entity.tick());
+    }
+    /**
+     * Spawn a new Fruit on the map
+     * @return false, if there are less than 5 tiles free
+     */
+    public boolean spawnEntity(Entity e) {
+        /* A buffer to prevent pseudo infinite loop for the last free tiles */
+        int lastBuffer = 5;
+        if(__container.size() > ((__x*__y)-5)) return false;
+        do {
+            rx = __random.nextInt(__x);
+            ry = __random.nextInt(__y);
+        } while(__field[rx][ry] != null);
+        this.place(new Point(rx,ry), e);
+        return true;
     }
 }
