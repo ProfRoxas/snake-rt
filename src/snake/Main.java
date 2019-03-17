b5f68744e3c1134cc338a41210e4f4bb2af5130d
ProfRoxas <kuroda.taiki@gmail.com>
2019-03-17 19:48:41 +0100

    Added Timer

--- src/snake/Main.java
+++ src/snake/Main.java
@@ -2,6 +2,8 @@ package snake;
 
 import java.awt.Canvas;
 import java.awt.EventQueue;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 
@@ -11,12 +13,15 @@ import snake.tools.Logic;
 class Main extends JFrame {
     private Canvas __canvas;
     private Logic __gameLogic;
+    private static Timer __timer;
+    private int __fps = 60;
 
     public Main() {
         System.out.println("Test");
         initUI();
     }
     private void initUI() {
+        //TODO: Add the Actual Canvas
         //add(__canvas);
 
         setTitle("Snake RT");
@@ -26,6 +31,23 @@ class Main extends JFrame {
 
         __gameLogic = new Logic(this);
         addKeyListener(new KeyEventListener(/* passing gameLogic later*/));
+        __timer = new Timer();
+        Main window = this;
+        TimerTask __timertask = new TimerTask(){
+            private int frame = 0;
+            private long lasttime = 0;
+            @Override
+            public void run() {
+                frame++;
+                long newtime = System.nanoTime();
+                window.setTitle("Hello World "+frame+", frametime: "+(newtime-lasttime)/1000000.0f+"ms");
+                lasttime = newtime;
+
+                //TODO: Update GameLogic
+                __canvas.repaint();
+            }
+        };
+        __timer.schedule( __timertask, 0, 1000/__fps);
     }
 
     public static void main(String[] args) {
