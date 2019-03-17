f9f68096bae6b0a58ab358f5974afcfc15dcf0f3
ProfRoxas <kuroda.taiki@gmail.com>
2019-03-17 15:04:06 +0100

    Added basic Fruit

---
+++ src/snake/entities/Fruit.java
@@ -0,0 +1,28 @@
+package snake.entities;
+
+public class Fruit implements Entity {
+    private FruitTypes __type;
+    
+    public static enum FruitTypes {
+        Basic
+    }
+    
+    public Fruit(FruitTypes ft) {
+        __type = ft;
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void collidesWith(Entity e) {
+
+    }
+
+    @Override
+    public String toString() {
+        return __type.toString();
+    }
+}
