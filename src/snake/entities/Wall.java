1cd8a516fad4582d1561725d4d8155e6b50e96cf
ProfRoxas <kuroda.taiki@gmail.com>
2019-03-22 11:21:24 +0100

    added basic Wall

---
+++ src/snake/entities/Wall.java
@@ -0,0 +1,13 @@
+package snake.entities;
+
+public class Wall implements Entity {
+
+    @Override
+    public void tick() {
+
+    }
+    @Override
+    public String toString() {
+        return "Wall";
+    }
+}
